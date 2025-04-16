eddd196d436f9e63b65055056ff61591e4bac3d7
MugishaBienvenu <mugishajeanbienvenu123@gmail.com>
2025-04-16 18:02:51 +0200

    sum_of_element

---
+++ sum_of_element.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c,d,e;
+
+    cout<<"Enter five numbers that are separated :"<<endl;
+    cin>>a>>b>>c>>d>>e;
+
+    int sum =a+b+c+d+e;
+    cout<<sum <<endl;
+}
