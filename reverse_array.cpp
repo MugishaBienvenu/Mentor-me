a3e0e267ef32c0a64e742c61a76e6a9e6169e51d
MugishaBienvenu <mugishajeanbienvenu123@gmail.com>
2025-04-16 22:05:41 +0200

    reverse_array

---
+++ reverse_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int my_array[10];
+
+    cout << "Enter ten numbers from 1 to 10 are separated : \n";
+    cin >> my_array[10];
+
+    for (int i = 10; i >0; i--)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
